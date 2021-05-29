7880f859ee3dc23438f72deb484b758a30e5243e
Steven <hi@stevenaanen.com>
2021-05-29 22:24:53 +0200

    System settings

--- defaults.sh
+++ defaults.sh
@@ -1,2 +1,6 @@
 defaults write -g InitialKeyRepeat -int 12
 defaults write -g KeyRepeat -int 1
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+
+git config --global push.default simple
+
